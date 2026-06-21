c0b75479c477283975931f6b3e4357221abe41d2
Dylan Dequanjae Howard <Dylan.D.M.Howard@proton.me>
2026-06-21 13:53:17 -0700

    Add files via upload

---
+++ al67x_physics/al67x_physics/pkg/al67x_physics_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const check_circle_collisions: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number) => any;
+export const check_magnet_zone: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number) => any;
+export const dist: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_start: () => void;
